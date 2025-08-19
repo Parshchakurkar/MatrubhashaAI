c2def94c40c9b5544ddeb4af523dbb9280831295
parshchakurkar <parshchakurkar@gmail.com>
2025-08-19 22:37:45 +0530

    updated the script

--- terraform/createagent.sh
+++ terraform/createagent.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 set -e
+echo "creating user...."
+useradd -ms /bin/bash azureuser && \
+echo "azureuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+passwd -d azureuser 
+su - azureuser
+sudo mkdir /azagent && cd /azagent
+
+# Download Azure Pipelines agent
+echo "downloading package....."
+sudo curl -LsS https://download.agent.dev.azure.com/agent/4.259.0/vsts-agent-linux-x64-4.259.0.tar.gz -o agent.tar.gz
+
+#changing the user permissions
+sudo chown -R $(whoami):$(whoami) . &&  chmod -R u+rwX .
+tar -xzf agent.tar.gz && sudo rm agent.tar.gz
+
+#install dependencies
+echo "install dependencies"
+sudo ./bin/installdependencies.sh
+
+chmod +x ./config.sh ./run.sh ./env.sh ./bin/Agent.Listener
 
 # Variables (replace with Key Vault/secure injection in production)
 AZP_URL="https://dev.azure.com/parshchakurkar"
-AZP_TOKEN="put-access-tokenhere "
+AZP_TOKEN="5IqbkLPVFHLhqCH99JvKoMtuP66RWdVXkMJoC0Px0VNEALjW8xh8JQQJ99BHACAAAAAAAAAAAAASAZDOIRVG"
 AZP_POOL="Default"
 AZP_AGENT_NAME=linuxagent
-
-# Install dependencies
-sudo apt-get update
-sudo apt-get install -y curl tar git
-
-# Create agent directory
-mkdir /azp && cd /azp
-
-# Download Azure Pipelines agent
-curl -LsS https://download.agent.dev.azure.com/agent/4.259.0/vsts-agent-osx-x64-4.259.0.tar.gz -o agent.tar.gz
-tar -xzf agent.tar.gz && rm agent.tar.gz
 
 # Configure agent
 ./config.sh --unattended \
@@ -31,3 +40,5 @@ tar -xzf agent.tar.gz && rm agent.tar.gz
 # Install and start as service
 sudo ./svc.sh install
 sudo ./svc.sh start
+
+echo "done....."
